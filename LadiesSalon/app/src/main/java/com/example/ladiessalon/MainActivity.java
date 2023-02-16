9bcd59dbba9bafd1f9ce3d775e9bc7e807161455
Prashant Sahatiya <prashantsahatiya18@gmail.com>
2023-02-16 10:46:15 +0530

    Uploaded new projects

---
+++ LadiesSalon/app/src/main/java/com/example/ladiessalon/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.ladiessalon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login,register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.signin);
+        register = findViewById(R.id.signup);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
