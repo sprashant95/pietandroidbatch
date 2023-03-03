0708bf567ab041ac001743ec44a3e7e470c7bec3
Prashant Sahatiya <prashantsahatiya18@gmail.com>
2023-03-03 08:36:26 +0530

    Updated BottomNavigation

--- PIETBottomNavigation/app/src/main/java/com/example/pietbottomnavigation/HomeFragment.java
+++ PIETBottomNavigation/app/src/main/java/com/example/pietbottomnavigation/HomeFragment.java
@@ -2,63 +2,56 @@ package com.example.pietbottomnavigation;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public HomeFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        ListView listView;
+
+        String[] moviename = {"Antman", "Antman and WASP", "Hulk", "Doctor Strange", "Captain America",
+                "Black Widow", "Black Panther", "Avengers",
+                "Antman", "Antman and WASP", "Hulk", "Doctor Strange", "Captain America",
+                "Black Widow", "Black Panther", "Avengers",
+                "Antman", "Antman and WASP", "Hulk", "Doctor Strange", "Captain America",
+                "Black Widow", "Black Panther", "Avengers"};
+
+        Integer[] movieimage = {R.drawable.antman,R.drawable.antman2,R.drawable.hulk,R.drawable.doctorstrange,
+                R.drawable.captainamerica,R.drawable.blackwidow,R.drawable.blackpanther,R.drawable.avengers,
+                R.drawable.antman,R.drawable.antman2,R.drawable.hulk,R.drawable.doctorstrange,
+                R.drawable.captainamerica,R.drawable.blackwidow,R.drawable.blackpanther,R.drawable.avengers,
+                R.drawable.antman,R.drawable.antman2,R.drawable.hulk,R.drawable.doctorstrange,
+                R.drawable.captainamerica,R.drawable.blackwidow,R.drawable.blackpanther,R.drawable.avengers};
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        listView = view.findViewById(R.id.listview);
+
+        MyHomeAdapter myHomeAdapter = new MyHomeAdapter(getActivity(),moviename,movieimage);
+        listView.setAdapter(myHomeAdapter);
+
+        return view;
+
     }
 }
---
+++ PIETBottomNavigation/app/src/main/java/com/example/pietbottomnavigation/MyHomeAdapter.java
@@ -0,0 +1,48 @@
+package com.example.pietbottomnavigation;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyHomeAdapter extends ArrayAdapter<String> {
+
+
+    Activity context;
+    String[] name;
+    Integer[] image;
+
+    public MyHomeAdapter(@NonNull Activity context, String[] name, Integer[] image) {
+        super(context, R.layout.mycustomitem);
+        this.context = context;
+        this.name = name;
+        this.image = image;
+    }
+
+    @Override
+    public int getCount() {
+        return name.length;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.mycustomitem,null);
+
+        ImageView imageView = rowView.findViewById(R.id.imageView);
+        TextView textView = rowView.findViewById(R.id.textView);
+
+        imageView.setImageResource(image[position]);
+        textView.setText(name[position]);
+
+        return rowView;
+    }
+}
